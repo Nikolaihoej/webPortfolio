8c62825cd505ca94f1bd7947745dcc59ddc27e09
Nikolai <nikolaihjensen@hotmail.com>
2025-10-25 17:08:14 +0200

    last update for now

--- src/components/ProjectCard.vue
+++ src/components/ProjectCard.vue
@@ -1,17 +1,17 @@
 <template>
-  <div class="card" @click="toggleDetails">
+  <div class="card" @mouseenter="showDetailsOnHover" @mouseleave="hideDetailsOnHover">
     <div class="card-inner">
       <div class="image-container">
         <img :src="image" :alt="title">
       </div>
       <div class="content-container">
         <div :class="['details-wrapper', detailsAnimClass]" @animationend="onDetailsAnimationEnd">
-          <h3 class="card-title">{{ title }}</h3>
           <div v-if="showDetails" class="details-content">
+            <h3 class="card-title">{{ title }}</h3>
             <p>{{ description }}</p>
-            <div class="tags">
+            <!-- <div class="tags">
               <span class="individuel-tag" v-for="tag in tags" :key="tag">{{ tag }}</span>
-            </div>
+            </div> -->
             <a :href="link" class="btn" target="_blank" @click.stop>View Project</a>
           </div>
         </div>
@@ -33,13 +33,13 @@ defineProps({
 const showDetails = ref(false);
 const detailsAnimClass = ref('');
 
-function toggleDetails() {
-  if (!showDetails.value) {
-    detailsAnimClass.value = 'animate__animated animate__slideInUp';
-    showDetails.value = true;
-  } else {
-    detailsAnimClass.value = 'animate__animated animate__slideOutDown';
-  }
+function showDetailsOnHover() {
+  detailsAnimClass.value = 'animate__animated animate__slideInUp';
+  showDetails.value = true;
+}
+
+function hideDetailsOnHover() {
+  detailsAnimClass.value = 'animate__animated animate__slideOutDown';
 }
 
 function onDetailsAnimationEnd() {
@@ -52,86 +52,96 @@ function onDetailsAnimationEnd() {
 
 <style scoped>
 .card {
-    margin: 18px;
-    width: 100%;
-    max-width: 400px;
-    aspect-ratio: 3 / 4;
-    border-radius: 8px;
-    box-shadow: 0 20px 40px rgba(0, 0, 0, 0.25), 0 10px 20px rgba(0, 0, 0, 0.15);
-    overflow: hidden;
-    cursor: pointer;
+  margin: 18px;
+  width: 100%;
+  max-width: 400px;
+  aspect-ratio: 3 / 4;
+  border-radius: 8px;
+  box-shadow: 0 20px 40px rgba(0, 0, 0, 0.25), 0 10px 20px rgba(0, 0, 0, 0.15);
+  overflow: hidden;
 }
 
 .card-inner {
-    display: flex;
-    flex-direction: column;
-    width: 100%;
-    height: 100%;
-    z-index: 1;
+  display: flex;
+  flex-direction: column;
+  width: 100%;
+  height: 100%;
+  z-index: 1;
 }
 
 .image-container {
-    position: relative;
-    width: 100%;
-    height: 100%;
-    border-radius: 16px;
+  position: relative;
+  width: 100%;
+  height: 100%;
+  border-radius: 16px;
 }
 
 .image-container img {
-    position: absolute;
-    width: 100%;
-    height: 538px;
-    object-fit: cover;
-    z-index: 1;
-    padding: 0;
+  position: absolute;
+  width: 100%;
+  height: 538px;
+  object-fit: cover;
+  z-index: 1;
 }
 
 .content-container {
-    padding: 12px 16px;
-    flex-grow: 1;
-    display: flex;
-    flex-direction: column;
-    justify-content: space-between;
-    z-index: 2;
+  padding: 12px;
+  flex-grow: 1;
+  display: flex;
+  z-index: 2;
 }
 
 .details-wrapper {
-    --animate-duration: 0.5s;
+  --animate-duration: 0.4s;
 }
 
 .content-container h3 {
-    font-size: 1.6rem;
-    line-height: 1.4;
-    font-weight: 600;
+  font-size: 2rem;
+  line-height: 1.5;
+  font-weight: 600;
 }
 
 .content-container p {
-    font-size: 1rem;
-    line-height: 1.4;
-    font-weight: 500;
-}
-
-.tags {
-    margin-top: 8px;
-}
-
-.individuel-tag{
-    padding: 4px 8px;
-    border-radius: 8px;
-    margin-right: 6px;
-    background-color: #414040;
-    color: #fff;
+  font-size: 1rem;
+  line-height: 1.5;
+  font-weight: 500;
 }
 
 .btn {
-    display: inline-block;
-    padding: 10px 16px;
-    background-color: #414040;
-    color: #fff;
-    text-decoration: none;
-    border-radius: 8px;
-    text-align: center;
-    font-weight: 600;
-    margin-top: 12px;
+  display: inline-block;
+  padding: 10px 16px;
+  color: #fff;
+  text-decoration: none;
+  text-align: center;
+  font-weight: 600;
+  margin-top: 12px;
+  text-transform: uppercase;
+  line-height: 1.2em;
+  letter-spacing: 2.5px;
+  fill: #FFFFFF;
+  color: #FFFFFF;
+  background-color: #4141418A;
+  border: 2px solid #FFFFFFC4;
+  border-radius: 2px 2px 2px 2px;
+  box-shadow: 0px 0px 10px 0px rgba(0, 0, 0, 0.5);
+  transition: all 0.3s ease;
 }
+
+.btn:hover {
+  background-color: #414040;
+  border-color: #FFFFFF;
+  box-shadow: 0px 0px 15px 0px rgba(0, 0, 0, 0.7);
+}
+
+/* .tags {
+  margin-top: 8px;
+}
+
+.individuel-tag{
+  padding: 4px 8px;
+  border-radius: 8px;
+  margin-right: 6px;
+  background-color: #414040;
+  color: #fff;
+} */
 </style>
--- src/components/Projects.vue
+++ src/components/Projects.vue
@@ -13,21 +13,21 @@ const projects = [
     title: 'Portfolio Website',
     description: 'This portfolio built with Vue 3. and deployed on firebase hosting.',
     image: placeholderImg,
-    tags: ['Vue3 ', 'JavaScript ', 'CSS '],
+    // tags: ['Vue3 ', 'JavaScript ', 'CSS '],
     link: 'https://yourportfolio.com'
   },
   {
     title: 'Todo App',
     description: 'A simple todo app built with Vue 3, MongoDB and Mongoose.',
     image: placeholderImg,
-    tags: ['Vue3 ', 'MongoDB ', 'Node.js '],
+    // tags: ['Vue3 ', 'MongoDB ', 'Node.js '],
     link: 'https://yourweatherapp.com'
   },
   {
     title: 'Appsheet App',
     description: 'A simple Low-code app built for Total Ejendom Service, made with the intent of Optimizing internal processes.',
     image: placeholderImg,
-    tags: ['Appsheet ', 'AppsheetDB ', 'Automation (AI)'],
+    // tags: ['Appsheet ', 'AppsheetDB ', 'Automation (AI)'],
     link: 'https://yourweatherapp.com'
   }
   // Add more projects as needed
